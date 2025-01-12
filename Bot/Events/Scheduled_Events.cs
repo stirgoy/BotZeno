10d83c1465885bd98ba1c7189b70bfb448cd76ff
Enrique Rodriguez <stirgoy@hotmail.com>
2025-01-12 14:50:28 +0100

    sort fix

--- Bot/Events/Scheduled_Events.cs
+++ Bot/Events/Scheduled_Events.cs
@@ -20,12 +20,36 @@ namespace Zeno
 
         }
 
+        private async Task EventUserAdd(Cacheable<SocketUser, RestUser, IUser, ulong> user, SocketGuildEvent server_event)
+        {
+            Print($"New user added {user.Id} on event {server_event.Id}");
 
+            string[] event_data = GetEvent(server_event.Id.ToString());
+            List<string[]> evnts = Config.Events_Noticed;
+
+            if (!event_data.Contains(user.Id.ToString()))
+            {
+                for (int i = 0; i < evnts.Count; i++)
+                {
+                    if (evnts[i][0] == server_event.Id.ToString())
+                    {
+                        List<string> newlist = evnts[i].ToList();
+                        newlist.Add(user.Id.ToString());
+                        evnts[i] = newlist.ToArray();
+                        Config.Events_Noticed = evnts;
+                        await Config_Save();
+                        break;
+                    }
+                }
+            }
+
+
+        }
 
 
         private async Task EventUserRemove(Cacheable<SocketUser, RestUser, IUser, ulong> user, SocketGuildEvent server_event)
         {
-            Print($"New user removed on event {user.Id}");
+            Print($"New user removed {user.Id} on event {server_event.Id}");
 
             List<string[]> evnts = Config.Events_Noticed;
 
--- Bot/Fnc_Zenos.cs
+++ Bot/Fnc_Zenos.cs
@@ -230,31 +230,7 @@ namespace Zeno
         /********************
                 EventUserAdd
         *////////////////////
-        private async Task EventUserAdd(Cacheable<SocketUser, RestUser, IUser, ulong> user, SocketGuildEvent server_event)
-        {
-            Print($"New user added on event {user.Id}");
-
-            string[] event_data = GetEvent(server_event.Id.ToString());
-            List<string[]> evnts = Config.Events_Noticed;
-
-            if (!event_data.Contains(user.Id.ToString()))
-            {
-                for (int i = 0; i < evnts.Count; i++)
-                {
-                    if (evnts[i][0] == server_event.Id.ToString())
-                    {
-                        List<string> newlist = evnts[i].ToList();
-                        newlist.Add(user.Id.ToString());
-                        evnts[i] = newlist.ToArray();
-                        Config.Events_Noticed = evnts;
-                        await Config_Save();
-                        break;
-                    }
-                }
-            }
-
-
-        }
+        
 
         /********************
                 Delete_Event
