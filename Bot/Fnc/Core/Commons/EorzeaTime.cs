7699166ecbd49cb6fc727255c473cea1065a6d4f
Enrique Rodriguez <stirgoy@hotmail.com>
2025-01-13 12:15:11 +0100

    eozea timer

---
+++ Bot/Fnc/Core/Commons/EorzeaTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zeno
+{
+    public static class EorzeaDateTimeExtention
+    {
+        public static DateTime EorzeaTime(this DateTime date)
+        {
+            const double EORZEA_MULTIPLIER = 3600D / 175D;
+
+            // Calculate how many ticks have elapsed since 1/1/1970
+            long epochTicks = date.ToUniversalTime().Ticks - (new DateTime(1970, 1, 1).Ticks);
+
+            // Multiply those ticks by the Eorzea multipler (approx 20.5x)
+            long eorzeaTicks = (long)Math.Round(epochTicks * EORZEA_MULTIPLIER);
+
+            return new DateTime(eorzeaTicks);
+        }
+    }
+}
